41212ee4335b30a47658dde3044644059d0b0925
kosi.siteadmin <sitemaster@kosicollaborative.com>
2023-11-15 22:13:23 +0545

    add tina

---
+++ tina/config.ts
@@ -0,0 +1,58 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+// const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+const branch = "main";
+
+export default defineConfig({
+  branch,
+  clientId: process.env.TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "images",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "project",
+        label: "Projects",
+        path: "content/english/projects",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "image",
+            name: "image",
+            label: "Image",
+            required: true,
+          },
+          {
+            type: "number",
+            name: "weight",
+            label: "Order",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
